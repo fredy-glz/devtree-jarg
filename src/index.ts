db3e4f1352e43f6af5f90f67e8d04a6f58a423f2
Alfredo Romero <fredyglz.fr@gmail.com>
2025-04-06 16:59:18 -0500

    Commit para validar que se cambio correctamente el usuario de git

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import server from "./server";
 const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
-  console.log("Servidor Funcionando en el puerto: ", PORT);
+  console.log("Servidor funcionando en el puerto: ", PORT);
 });
