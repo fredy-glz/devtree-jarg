eba36e387a13eed364e1113766f519b0dfb7ba30
Alfredo Romero <jromero-ext@ultratech-inc.com>
2025-04-06 16:23:47 -0500

    Sección 3: devtree - routing, ESM, y dependencias

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hola mundo desde devtree");
+// const express = require('express'); // CommonJS
+import express from "express"; // ESModule
+const app = express();
+
+// Routing
+app.get("/", (req, res) => {
+  res.send("Hola Mundo en Express");
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log("Servidor Funcionando en el puerto: ", PORT);
+});
