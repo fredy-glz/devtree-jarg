84ce242eb315c33f41d745fd16371ebe288177ee
Alfredo Romero <fredyglz.fr@gmail.com>
2025-04-18 03:04:45 -0500

    Sección 6: Conección a la db

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 // const express = require('express'); // CommonJS
 import express from "express"; // ESModule
+import 'dotenv/config'
 import router from "./router";
+import { connectDB } from "./config/db";
 
 const app = express();
+
+connectDB();
 
 // Leer datos de formularios
 app.use(express.json());
