5193d2083b2bfefee681c61fcb63e2f40abbb81d
Alfredo Romero <fredyglz.fr@gmail.com>
2025-04-21 00:38:12 -0500

    Sección 8: devtree - Autenticación de usuarios

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,18 +1,10 @@
 import { Request, Response } from "express";
-import { validationResult } from "express-validator";
 import slug from "slug";
 
 import User from "../models/User";
-import { hashPassword } from "../utils/auth";
+import { checkPassword, hashPassword } from "../utils/auth";
 
 export const createAccount = async (req: Request, res: Response) => {
-  // Manejar errores
-  let errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    res.status(400).json({ errors: errors.array() });
-    return;
-  }
-
   const { email, password } = req.body;
 
   const userExists = await User.findOne({ email });
@@ -37,4 +29,26 @@ export const createAccount = async (req: Request, res: Response) => {
   await user.save();
 
   res.status(201).send("Registro Creado Correctamente");
+};
+
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  // Revisar si el usuario esta registrado
+  const user = await User.findOne({ email });
+  if (!user) {
+    const error = new Error("El Usuario no existe");
+    res.status(404).json({ error: error.message });
+    return;
+  }
+
+  // Comprobar el password
+  const isPasswordCorrect = await checkPassword(password, user.password);
+  if (!isPasswordCorrect) {
+    const error = new Error("Password Incorrecto");
+    res.status(401).json({ error: error.message });
+    return;
+  }
+
+  res.status(200).send("Autenticado...");
 };
