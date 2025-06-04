ea0bf453742ec73afa38244dfab039ab42106b0c
Alfredo Romero <fredyglz.fr@gmail.com>
2025-06-03 19:54:44 -0500

    Subiendo todo para cambio de equipo

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
 // const express = require('express'); // CommonJS
 import express from "express"; // ESModule
-import 'dotenv/config'
+import "dotenv/config";
+import cors from "cors";
+
 import router from "./router";
 import { connectDB } from "./config/db";
+import { corsConfig } from "./config/cors";
+
+connectDB();
 
 const app = express();
 
-connectDB();
+// CORS
+app.use(cors(corsConfig));
 
 // Leer datos de formularios
 app.use(express.json());
